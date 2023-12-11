5e592a14f203a338babc6d63e48c0c54371fe8ec
Augustin Cibotari <augustus.apso@gmail.com>
2023-12-11 16:37:41 +0200

    Add order routes and handler methods

--- handler/order.go
+++ handler/order.go
@@ -14,3 +14,15 @@ func (o *Order) Create(w http.ResponseWriter, r *http.Request) {
 func (o *Order) List(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "list order")
 }
+
+func (o *Order) GetByID(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "get order by id")
+}
+
+func (o *Order) UpdateByID(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "update order by id")
+}
+
+func (o *Order) DeleteByID(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "delete order by id")
+}
